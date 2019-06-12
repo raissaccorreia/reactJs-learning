f341f69dac29e3d469002b1d76fa52734eba09b8
Raissa Correia <raissa.correia@clickideia.com.br>
2019-06-12 14:24:44 -0300

    Inicio da pratica freeCodeCamp

---
+++ todolist/src/components/MyApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyApp(){
+    return (
+    <div>
+        <h1>Oie!</h1>
+        <p>hahahaha</p>
+        <ul>
+            <li>apple</li>
+            <li>honey</li>
+            <li>bee</li>
+        </ul>
+    </div>)
+}
+
+export default MyApp;
