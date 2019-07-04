bb109c237b7defdaa00a0d2024e95652359792aa
Raissa Correia <raisssa.correia@gmail.com>
2019-07-04 13:00:45 -0300

    ajusting success file

--- materialstepform/src/components/Success.jsx
+++ materialstepform/src/components/Success.jsx
@@ -2,16 +2,7 @@ import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar'
 
-export class Success extends Component {
-    continue = e => {
-        e.preventDefault();
-        // PROCESS FORM //
-        this.props.nextStep();
-    };    
-    back = e => {
-        e.preventDefault();
-        this.props.prevStep();
-    };
+export class Success extends Component {    
     render() {
         return (
             <MuiThemeProvider>
